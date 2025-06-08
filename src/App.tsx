a57f7d36e6f5403296cb2b3842baf33b9ffc794d
nguyentamdiep <tamdiepnp@gmail.com>
2025-06-08 10:18:13 +0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
+//import { useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState("")
+  useEffect(()=>{
+    const config = {
+    method: 'GET',
+    url: 'https://script.google.com/macros/s/AKfycbx2gKw2Rh86TXHV_iut4nVIWsc8p1A7NFwhhVqO6AokGa5og-pHOS9tbFMhkXhvbE12fw/exec'
+  }
+  axios(config).then((res)=>{
+    console.log("test");
+    console.log(res.data[1][0]);
+    setText(res.data[1][0])
+  })
 
+
+  }, []);
+
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Hello World 123</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>hello world!</h1>
+      <p>{text}</p>
+    </div>
   )
 }
 
